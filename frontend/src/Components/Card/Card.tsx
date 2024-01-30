87c635c8632b0b5b500db21676ab8720c9217be1
quang-khuong-duy-le <nathanle2711@gmail.com>
2024-01-29 23:32:34 -0500

    Add card list

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,18 +1,24 @@
 import "./Card.css";
 
-type Props = {}
+type Props = {
+  companyName: string;
+  ticker: string;
+  price: number;
+};
 
-const Card = (props: Props) => {
+const Card = ({ companyName, ticker, price }: Props) => {
   return (
-    <div className='card'>
-        <img src='' alt='Image' />
-        <div className='details'>
-            <h2>AAPL</h2>
-            <p>$100</p>
-        </div>
-        <p className='info'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid at tempora culpa quibusdam quod dolor aspernatur nihil ipsum sequi iste dolorem, in qui temporibus reprehenderit consequatur non? Optio, debitis voluptatibus!</p>
+    <div className="card">
+      <img src="" alt="Image" />
+      <div className="details">
+        <h2>{companyName} ({ticker})</h2>
+        <p>${price}</p>
+      </div>
+      <p className="info">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit!
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;

69ccab2fbd7e4765a81db0cd6e47077a97b4d7b0
quang-khuong-duy-le <nathanle2711@gmail.com>
2024-02-13 23:02:23 -0500

    Create company tile

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { Outlet, useParams } from "react-router";
 import { CompanyProfile } from "../../interfaces";
 import { getCompanyProfile } from "../../Apis/StockApi";
 import SideBar from "../../Components/SideBar/SideBar";
 import CompanyDashboard from "../../Components/CompanyDashboard/CompanyDashboard";
+import Tile from "../../Components/Tile/Tile";
 
 type Props = {};
 
@@ -25,7 +26,9 @@ const CompanyPage = (props: Props) => {
       {company ? (
         <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
           <SideBar />
-          <CompanyDashboard />
+          <CompanyDashboard>
+            <Tile title="Company Name" subTitle={company.companyName} />
+          </CompanyDashboard>
         </div>
       ) : (
         <div>Company not found!</div>

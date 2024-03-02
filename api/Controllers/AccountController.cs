17b33ace84122881015e36b8764b8d92ddd7ef14
quang-khuong-duy-le <nathanle2711@gmail.com>
2024-03-02 14:49:54 -0500

    Add register route

---
+++ api/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+using api.Dtos.Account;
+using api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/account")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest registerRequest)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var appUser = new AppUser
+                {
+                    UserName = registerRequest.Username,
+                    Email = registerRequest.Email,
+                };
+
+                var createdUser = await _userManager.CreateAsync(appUser, registerRequest.Password);
+                if (createdUser.Succeeded)
+                {
+                    var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
+                    if (roleResult.Succeeded)
+                    {
+                        return Ok("User created");
+                    }
+                    else
+                    {
+                        return StatusCode(500, roleResult.Errors);
+                    }
+                }
+                else 
+                {
+                    return StatusCode(500, createdUser.Errors);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
+        }
+    }
+}

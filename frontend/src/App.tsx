6aeeacdecaa63c5a8f3fd36f97ac0d8a04c5b823
quang-khuong-duy-le <nathanle2711@gmail.com>
2024-03-06 23:39:11 -0500

    Add auth services

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router";
 import NavBar from "./Components/NavBar/NavBar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
       <NavBar />
       <Outlet />
+      <ToastContainer />
     </>
   );
 }
---
+++ frontend/src/Services/AuthService.tsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { handleApiError } from "../Helpers/ApiErrorHandler";
+import { UserProfileToken } from "../Models/User";
+
+const api = "http://localhost:5167/api";
+
+export const loginApi = async (username: string, password: string) => {
+  try {
+    const data = await axios.post<UserProfileToken>(api + "account/login", {
+      username: username,
+      password: password,
+    });
+
+    return data;
+  } catch (error) {
+    handleApiError(error);
+  }
+};
+
+export const registerApi = async (email: string, username: string, password: string) => {
+    try {
+      const data = await axios.post<UserProfileToken>(api + "account/register", {
+        email: email,
+        username: username,
+        password: password,
+      });
+  
+      return data;
+    } catch (error) {
+      handleApiError(error);
+    }
+  };
